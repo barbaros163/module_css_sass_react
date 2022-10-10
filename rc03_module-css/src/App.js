5c19f91c06a48c327b2f69f15111048944d2086e
Yasin Yavuz <barbaros1675@gmail.com>
2022-10-10 22:35:33 +0200

    structure completed

--- rc03_module-css/src/App.js
+++ rc03_module-css/src/App.js
@@ -1,5 +1,18 @@
+import Card from "./components/card/Card";
+import data from "./util/data";
+
 function App() {
-  return <h1>APP</h1>;
+  // JS
+
+  return (
+    //JSX
+    <>
+      {data.map((item) => {
+        const { id, language, img, btnName } = item;
+        return <Card />;
+      })}
+    </>
+  );
 }
 
 export default App;
