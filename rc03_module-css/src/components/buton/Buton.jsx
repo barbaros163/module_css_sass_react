3b3ba9b4549003d81f8f0eb08643048021bad62a
Yasin Yavuz <barbaros1675@gmail.com>
2022-10-11 13:19:38 +0200

    module done

--- rc03_module-css/src/components/buton/Buton.jsx
+++ rc03_module-css/src/components/buton/Buton.jsx
@@ -1,7 +1,20 @@
-import "./Buton.css";
+// import "./Buton.css";
+import ButonStyle from "./buton.module.css";
 
-const Buton = () => {
-  return <div>Buton</div>;
+//* Eğer CSS'deki class adi kebap-case seklinde yazilmis ise
+//* module ile bu class'a erismek icin ornekte goruldugu gibi
+//* koseli parentez ile erisim yapilir.Koseli parentez erisimi
+//* en garanti yontemdir.
+
+//* ORN:moduleName["class-adi"]
+
+const Buton = ({ btn }) => {
+  console.log(btn);
+  return (
+    <div className={ButonStyle.title}>
+      <button className={ButonStyle["btn-blue"]}>{btn}</button>
+    </div>
+  );
 };
 
 export default Buton;
