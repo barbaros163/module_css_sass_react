8d16414bdb8adb0b2ad5323b322049625167f197
Yasin Yavuz <barbaros1675@gmail.com>
2022-11-29 13:31:09 +0100

    Butonstyle

--- rc03_module-css/src/components/buton/Buton.jsx
+++ rc03_module-css/src/components/buton/Buton.jsx
@@ -1,11 +1,11 @@
 // import "./Buton.css";
 
-import "./Buton.css";
+import ButonStyle from "./buton.module.css";
 
 const Buton = ({ btn }) => {
   return (
-    <div>
-      <button> {btn} </button>
+    <div className={ButonStyle.title}>
+      <button className={ButonStyle["btn-blue"]}> {btn} </button>
     </div>
   );
 };
--- rc03_module-css/src/components/card/Card.jsx
+++ rc03_module-css/src/components/card/Card.jsx
@@ -1,13 +1,13 @@
 import Buton from "../buton/Buton";
-import "./Card.css";
+import CardStyle from "./card.module.css";
 
 const Card = ({ language, btn, img }) => {
   // console.log(props);
   // const {language, btn, img} = props; //! destr.
   return (
-    <div>
+    <div className="title">
       <h1>{language}</h1>
-      <img src={img} alt="img" />
+      <img className="images" src={img} alt="img" />
       <Buton btn={btn} />
     </div>
   );
