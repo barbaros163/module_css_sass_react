cb80eef5d5a19733d03aeb47120b416823bd7aa6
Yasin Yavuz <barbaros1675@gmail.com>
2022-11-29 15:18:09 +0100

    title

--- rc03_module-css/src/components/card/Card.jsx
+++ rc03_module-css/src/components/card/Card.jsx
@@ -5,9 +5,9 @@ const Card = ({ language, btn, img }) => {
   // console.log(props);
   // const {language, btn, img} = props; //! destr.
   return (
-    <div className="title">
-      <h1>{language}</h1>
-      <img className="images" src={img} alt="img" />
+    <div>
+      <h1 className={CardStyle["title"]}>{language}</h1>
+      <img className={CardStyle["images"]} src={img} alt="img" />
       <Buton btn={btn} />
     </div>
   );
